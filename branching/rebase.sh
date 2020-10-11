bf71a5c0284a06114c70f367fe9df58ed43cba1c
BobylevEug <BobylevEug@yandex.ru>
2020-10-11 15:24:06 +0500

    git-rebase1 delete

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,6 +3,12 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> 4ca8c90... git-rebase1
     count=$(( $count + 1 ))
 done
+
+echo "====="
