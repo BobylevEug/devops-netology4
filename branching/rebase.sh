8a90d4e1e5114fe71b71b8d7b4a6a6586caa9566
BobylevEug <BobylevEug@yandex.ru>
2020-10-12 19:25:54 +0500

    2 conflict

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # display command line options
-
+  
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
-=======
-    echo "Parameter: $param"
->>>>>>> 4ca8c90... git-rebase1
     count=$(( $count + 1 ))
 done
 
